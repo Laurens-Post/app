c30262f3cc078af4502efa5b1cc4a07fd389e82f
Laurens-Post <contact@lphosting.nl>
2019-10-15 00:08:56 +0200

    Using states & events

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return (
-      <View style={styles.screen}>
-        <View style={styles.inputContainer}>
-          <TextInput placeholder="Doelen voor mps app" style={styles.input} />
-          <Button title="Toevoegen" />
+    const [enteredGoal, setEnteredGoal] = useState('');
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    };
+
+    const addGoalHandler = () => {
+        console.log(enteredGoal);
+    };
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Doelen voor maps app"
+                    style={styles.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <Button title="Toevoegen" onPress={addGoalHandler} />
+            </View>
+            <View />
         </View>
-        <View />
-      </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  }
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10
+    }
 });
