be8e3b92900408eaac4071fd702159d8e0a45db0
Laurens-Post <contact@lphosting.nl>
2019-10-14 15:54:41 +0200

    Starting with a small app for setting goals for the map app

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Test');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Verander de tekst" onPress={() => setOutputText('De tekst is veranderd')} />
-    </View>
+      <View style={{ padding: 50 }}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+          <TextInput
+              placeholder="Doelen voor maps app"
+              style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}
+          />
+          <Button title="ADD" />
+        </View>
+        <View />
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
