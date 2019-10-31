6a1b07fd462c2f466d2904d577556365a5670f97
Laurens-Post <contact@lphosting.nl>
2019-10-31 10:21:57 +0100

    Starting with setting up login for user authentication

---
+++ user/AuthScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    ScrollView,
+    View,
+    KeyboardAvoidingView,
+    StyleSheet,
+    Button
+} from 'react-native';
+
+import Input from '../../components/UI/Input';
+
+const AuthScreen = props => {
+
+    const styles = StyleSheet.create({
+    }
+
+    export default AuthScreen;
